94de34670677f608e284713756f301b4e2b36325
PharaohsVizier <PharaohsVizier@users.noreply.github.com>
2018-02-24 14:44:18 -0800

    Delete old version

--- DjangoAPI/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Decision, Citation, Paragraph
-
-# Register your models here.
-admin.site.register(Decision)
-admin.site.register(Citation)
-admin.site.register(Paragraph)
