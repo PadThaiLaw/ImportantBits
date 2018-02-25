78740d0c48888cf263284227fe958f7f4e3ea3d8
PharaohsVizier <PharaohsVizier@users.noreply.github.com>
2018-02-24 20:16:25 -0800

    Updating for Heroku

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
